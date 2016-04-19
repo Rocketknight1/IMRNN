c2b3beae2514cf5b16e03d42213886f1889d24f8
Rocketknight1 <rocketknight1@gmail.com>
2016-04-19 21:17:17 +0100

    Vectorizing now works, need to trim the huge range of punctuation/special chars though.

--- title_encoder.py
+++ title_encoder.py
@@ -2,6 +2,7 @@
 
 import pdb,sys
 from title_extractor import ExtractTitles
+import numpy as np
 
 #We vectorize titles with a sliding-window strategy
 #with a minimum length of 1 character and a maximum length
@@ -14,30 +15,60 @@ from title_extractor import ExtractTitles
 #uses index n+1).
 
 def VectorizeTitle(title,char_to_index,end_index,max_len):
+	#Returns all training vectors that can be produced from a single title
+	#In all cases, the label is the final column
     training = []
-    labels = []
     title_indices = [char_to_index[char] for char in list(title)]
     title_indices.append(end_index)
     start_char = 0
-    target_char = 1
-    while target_char < len(title_indices):
-        training.append(title_indices[start_char:target_char-1])
-        labels.append(title_indices[target_char])
-        target_char = target_char+1
+    target_char = 2
+    while target_char <= len(title_indices):
+        training.append(title_indices[start_char:target_char])
+        target_char+=1
         if target_char - start_char > max_len:
             start_char+=1
-    return (training,labels)
+    return training
 
-def MakeTrainingData():
-    pass
+def OneHot(vector,end_index):
+	#returns a one-hot encoding of a characters vector with dimensions (num_timesteps,size_of_charset)
+	output = np.zeros((len(vector),end_index+1),dtype=np.uint8)
+	for i in range(len(vector)):
+		output[i,vector[i]]=1
+	return output
+	
+def ListToNumpy(sample_list,num_timesteps,end_index):
+    #Take a list of vectorized titles and return a 3D numpy array suitable for training from
+	output = np.zeros((len(sample_list),num_timesteps,end_index+1),dtype=np.uint8)
+	for i in range(len(sample_list)):
+		output[i,:,:] = sample_list[i]
+	return output
+	
+def MakeTrainingData(titles,char_to_index,end_index,max_len):
+    training_cases_by_length = dict()
+    for length in range(2,max_len+1):
+        training_cases_by_length[str(length)]=[]
+    for title in titles:
+        title_vectors = VectorizeTitle(title,char_to_index,end_index,max_len)
+        for vector in title_vectors:
+            training_cases_by_length[str(len(vector))].append(OneHot(vector,end_index)) 
+    for length in range(2,max_len+1):
+        training_cases_by_length[str(length)] = ListToNumpy(training_cases_by_length[str(length)],length,end_index)
+    return training_cases_by_length
+		
+MAX_LEN = 12
 
-
-titles = ExtractTitles(sys.argv[1])
-titlestring = ''.join(titles).lower() #We ignore case
+titles = ExtractTitles(sys.argv[1]) #Note case is ignored
+titlestring = ''.join(titles)
 titlechars = set(titlestring)
 
-char_to_index = {ch:i for i,ch in enumerate(charset)}
-index_to_char = {i:ch for i,ch in enumerate(charset)}
+char_to_index = {ch:i for i,ch in enumerate(titlechars)}
+index_to_char = {i:ch for i,ch in enumerate(titlechars)}
+
+end_index = max(index_to_char.keys())+1
+
+training = MakeTrainingData(titles,char_to_index,end_index,MAX_LEN)
+
+np.savez_compressed('training.npz',**training)
 
 
 
--- title_extractor.py
+++ title_extractor.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+import pdb
 
 def ExtractTitles(file):
     title_list = []
     f = open(file,encoding='latin_1')
     for line in f:
         if line[0]=='#':
-            title = line[line.index('"')+1:line.rindex('"')]
+            try:
+                title = line[2:line.rindex('(')]
+                title = title.rstrip('" ').lstrip('''#" ''').lower()
+            except:
+                pdb.set_trace()
             title_list.append(title)
     return title_list
         
